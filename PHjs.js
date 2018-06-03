f1b198e079c0e8fe4e8c245caa6e289ffb55a06e
Travis <tjxhahn@gmail.com>
2018-06-03 08:14:24 -1200

    Update PHjs.js

--- PHjs.js
+++ PHjs.js
@@ -26,7 +26,7 @@ const PH = {
     } else {
       let x = Math.floor(Math.random() * max);
       if (x < min) {
-        return x + min; 
+        return min; 
       } else {
         return x; 
       }
